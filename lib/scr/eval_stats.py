337d90d7b2d7ffc91db83ecc52e416c87709cb2f
Diego Fuentes Palacios <43205584+Dfupa@users.noreply.github.com>
2019-12-10 12:05:05 +0100

    Update eval_stats.py

--- lib/scr/eval_stats.py
+++ lib/scr/eval_stats.py
@@ -35,9 +35,11 @@ if __name__ == "__main__":
     ap.add_argument('--processes', default=1, type=int,
                     help='Number of processes to use in parallel.')
     ap.add_argument('--sniffles', type=bool, default=False,
-                   help='Parameter used to reformat the sniffles callset if provided')
+                   help='Parameter used to reformat the sniffles callset if provided. Default is \'False\'.')
     ap.add_argument('--plot', type=bool, default=False,
-                   help='Parameter used to produce a plot of the eval metrics for different svim score filtering')
+                   help='Parameter used to produce a plot of the eval metrics for different svim score filtering. Default is \'False\'.')
+    ap.add_argument('--iterator', type=int, default=20,
+                   help='Parameter used in the plotting step. It is the max svim filtering score range indicated from 0 to \'--iterator\'. Default is \'20\'.')
     ap.add_argument('-v', '--verbose', action='store_true',
                     help='Verbose (goes to stderr)')
     args = ap.parse_args()
@@ -47,6 +49,7 @@ if __name__ == "__main__":
     feature = args.svtype
     sniffles = args.sniffles
     plot = args.plot
+    iterator = args.iterator
 
     #Make sure the paths provided exist
     
@@ -70,7 +73,7 @@ if __name__ == "__main__":
     print("#############\n")
     owd = os.getcwd()
     nwd = os.path.dirname(os.path.abspath(calls_path))
-    print(nwd)
+    
     if owd == nwd:
         pass
     else:
@@ -106,7 +109,7 @@ if __name__ == "__main__":
         print(os.popen(command_svim_1).read())
  
     
-    def sniffles_reformat(callset, featuretype):
+    def sniffles_reformat(callset, featuretype, tempfile = 'sniffles_reformated.vcf'):
         """
         Reformat the sniffles vcf callset
         """    
@@ -114,7 +117,7 @@ if __name__ == "__main__":
 
             command_snif_1 = 'cat ' + os.path.abspath(callset) + ' | awk \'OFS="\\t" {{ if($1 ~ /^#/) {{ print $0 }} }}\' > '+os.path.join(nwd, 'header.tmp.vcf')
             command_snif_2 = 'cat ' + os.path.abspath(callset) + ' | awk \'OFS="\\t" {{ if($1 !~ /^#/) {{print $0}} }}\' | grep \"' + featuretype + '\" > '+os.path.join(nwd, 'subset.tmp.vcf')
-            command_snif_3 = 'cat '+os.path.join(nwd, 'header.tmp.vcf')+' '+os.path.join(nwd, 'subset.temp.vcf')+' > '+os.path.join(nwd, 'sniffles_reformated.vcf')
+            command_snif_3 = 'cat '+os.path.join(nwd, 'header.tmp.vcf')+' '+os.path.join(nwd, 'subset.temp.vcf')+' > '+os.path.join(nwd, tempfile)
             print(os.popen(command_snif_1).read(), os.popen(command_snif_2).read(), os.popen(command_snif_3).read())
             print(os.popen('rm *.tmp.vcf').read())
 
@@ -154,18 +157,19 @@ if __name__ == "__main__":
         
         return df
     
-    def plot_svim_filtering(truth, callset, featuretype, iterations=21):
+    def plot_filtering(truth, callset, featuretype, iterations=20):
                                                                            
         
         performance_matrix = np.zeros((iterations,3))
         
         if sniffles == False and plot == True:
-            
+            dataset = "Svim"
             print("############################################")
             print("#Optional Step: Creating the plot#")                                                                  
             print("############################################\n")
         
-            for i in range(0, iterations, 1):
+            for i in range(0, iterations+1, 1):
+                i = i - 1 #To avoid issues with Python's indexing
                 subcommand = 'grep -v \"hom_ref\" '+os.path.realpath(callset)+' | awk  \'OFS="\\t" {{ if($1 ~ /^#/) {{ print $0 }} else {{ if($6>='+str(i)+') {{ print $0 }} }} }}\' > '+os.path.join(nwd, 'filter.temp.vcf')
                 print(os.popen(subcommand).read())                                                                                                                                     
                 new_calls = os.path.abspath('filter.temp.vcf')                                                           
@@ -174,33 +178,52 @@ if __name__ == "__main__":
                 results = recall_precision_stats(truth, reformated_call)
                 sensitivity = results.iloc[0]['Results in %']
                 precision = results.iloc[1]['Results in %']    
-                f1 = 2*((sensitivity*precision)/(sensitivity+precision))
+                f1 = results.iloc[2]['Results in %']
                 #Store it in the performance matrix
                 performance_matrix[i, 0] = sensitivity
                 performance_matrix[i, 1] = precision
                 performance_matrix[i, 2] = f1
                 #Remove temp files
-                print(os.popen('rm filter.temp.vcf').read())
-                print(os.popen('rm temp.vcf').read())
-                print('*')
+                print(os.popen('rm filter.temp.vcf').read(), os.popen('rm temp.vcf').read(), 'Iteration: '+str(i+1)+'.')
+                #print(os.popen('rm temp.vcf').read())
+                
                 
             
             #Let's start to plot
-            print('\nNow lets plot it')
+            print('\nNow lets plot it\n')
             X = performance_matrix[:, 0]#Sensitivity values
             Y = performance_matrix[:, 1]#Precision values
             F = performance_matrix[:, 2]#F1 values
+
+
+            #Mean and standard deviation stored in a list. It will be added to the eval_stats.txt if plot == True
+            result_list = []
+            mean_recall = np.mean(X)
+            result_list.append(mean_recall)
+            mean_precision = np.mean(Y)
+            result_list.append(mean_precision)
+            mean_f1 = np.mean(F)
+            result_list.append(mean_f1)
+            std_recall = np.std(X)
+            result_list.append(std_recall)
+            std_precision = np.std(Y)
+            result_list.append(std_precision)
+            std_f1 = np.std(F)
+            result_list.append(std_f1)
             
+
+            #########PLOTS#########
             plt.figure(figsize=(20,14))
+            plt.title("Results for "+str(dataset)+" based on a range of quality score filtering ("+str(iterations)+" iterations).")
+            plt.grid()
             #Subplot 1
             plt.subplot(2,2,1)
             plt.title("Positive feedback mRNA")
             line1, = plt.plot(Y, X, 'o-', color="b")
-            plt.title("Evolution of the eval metrics based on SVIM score filtering (from >=0 to >=40, ascendant)")
             plt.xlabel('Precision')
-            plt.xticks(np.array(Y))
+            plt.xticks(np.arange(0, 0.4, 0.05))
             plt.ylabel('Sensitivity')
-            plt.yticks(np.array(X))
+            plt.yticks(np.arange(0.6, 1.05, 0.05))
             legend_handles = [ mlines.Line2D([], [], color='b', marker='o', \
                             markersize=10, label='Sensitivity-Precision trade-off')]
             plt.legend(handles=legend_handles, loc = 'best')
@@ -209,10 +232,12 @@ if __name__ == "__main__":
             plt.subplot(2,2,2)
             plt.title("Precision based on the Svim Q score filtering")
             line1, = plt.plot(np.arange(0, iterations), Y, 'o-', color="r")
+            plt.fill_between(np.arange(0, iterations), Y - std_precision / np.sqrt(10), \
+                         Y + std_precision / np.sqrt(10), alpha=0.1, color="green")
             plt.xlabel('Svim Q score selected')
             plt.xticks(np.arange(0, iterations))
             plt.ylabel('Precision')
-            plt.yticks(np.array(Y))
+            plt.yticks(np.arange(0, 0.45, 0.05))
             legend_handles = [ mlines.Line2D([], [], color='r', marker='o', \
                             markersize=10, label='Precision')]
             plt.legend(handles=legend_handles, loc = 'best')
@@ -221,10 +246,12 @@ if __name__ == "__main__":
             plt.subplot(2,2,3)
             plt.title("Sensitivity based on the Svim Q score filtering")
             line1, = plt.plot(np.arange(0, iterations), X, 'o-', color="g")
+            plt.fill_between(np.arange(0, iterations), X - std_recall / np.sqrt(10), \
+                         X + std_recall / np.sqrt(10), alpha=0.1, color="green")
             plt.xlabel('Svim Q score selected')
             plt.xticks(np.arange(0, iterations))
             plt.ylabel('Sensitivity')
-            plt.yticks(np.array(X))
+            plt.yticks(np.arange(0.6, 1.05, 0.05))
             legend_handles = [ mlines.Line2D([], [], color='g',marker='o', \
                             markersize=10, label='Sensitivity')]
             plt.legend(handles=legend_handles, loc = 'best')
@@ -232,19 +259,21 @@ if __name__ == "__main__":
             #Subplot 4
             plt.subplot(2,2,4)
             line1, = plt.plot(np.arange(0, iterations), F, 'o-', color="purple")
+            plt.fill_between(np.arange(0, iterations), F - std_f1 / np.sqrt(10), \
+                         F + std_f1 / np.sqrt(10), alpha=0.1, color="green")
             plt.xlabel('Svim Q score selected')
             plt.xticks(np.arange(0, iterations))
             plt.ylabel('F1 score')
-            plt.yticks(np.array(F))
+            plt.yticks(np.arange(0, 0.65, 0.05))
             legend_handles = [ mlines.Line2D([], [], color='purple',marker='o', \
                             markersize=10, label='F1 score')]
             plt.legend(handles=legend_handles, loc = 'best')
                               
             #Save the plot
-            plt.savefig(os.path.join(nwd, 'svim_filtering.png'))
+            plt.savefig(os.path.join(nwd, (str(dataset)+'_filtering'+str(featuretype)+'.png')))
 
-            
-            
+            return result_list
+
         else:
             pass
 
@@ -275,17 +304,29 @@ if __name__ == "__main__":
     print("############################################")
     print("#Step 3: Obtaining the precision and recall#")                                                                  
     print("############################################\n")                                                                       
-    results = recall_precision_stats(hq, new_call)
-    
+    try:
+        results = recall_precision_stats(hq, new_call)
+    except ValueError:
+        sys.exit(-1)
+    print("Everything went smoothly.\n")
     #This is an optional step, if the conditions are met, will produce a plot
-    check_plot =plot_svim_filtering(hq, new_call, feature)    
-          
+    try:
+        check_plot =plot_filtering(hq, new_call, feature, iterator)    
+    except ValueError:
+        sys.exit(-1)      
     file = open('eval_stats.txt', 'w')
     file.write("These are the results for the evaluation of "+os.path.basename(hq)+" truth dataset and "+os.path.basename(calls)+" callset dataset.\n")
     file.write("\n")
-    file.write(results.to_string(index=False))
+    if plot == True:
+        file.write("Note that the added columns 'Mean values' and 'Std values' stand for the mean and standard deviation of the iterative loop using a range of 0 to "+str(iterator)+" Q scores.\n")
+        results.insert(loc=2, column='Mean values', value=[check_plot[0],check_plot[1], check_plot[2]])
+        results.insert(loc=3, column='Std values', value=[check_plot[3],check_plot[4], check_plot[5]])
+        file.write("\n")
+        file.write(results.to_string(index=False))
+    else:
+        file.write(results.to_string(index=False))
     file.write("\n\nAnd it is done! Bye.\n")
     file.close()
-    print("Removing the temporary files, please wait ...\n")
+    print("\nRemoving the temporary files, please wait ...\n")
     print(os.popen('rm svim_reformated.vcf').read())
     print("And it is done! Bye.\n")
