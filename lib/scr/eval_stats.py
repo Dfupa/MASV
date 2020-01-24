854a2edd6daba656419323921f884c7940990ffa
Diego Fuentes Palacios <43205584+Dfupa@users.noreply.github.com>
2020-01-24 12:22:41 +0100

    Update eval_stats.py

--- lib/scr/eval_stats.py
+++ lib/scr/eval_stats.py
@@ -4,9 +4,12 @@
 #Contact email: diegofupa@gmail.com
 
 """
+Description:
+
 Uses bedtools intersect from a conda environment with access to shell language to calculate the Recall and Precision of the benchmarking. 
 To filter the vcf files, it was used vcffilter from vcflib for Sniffles filtering. After some tests, it proved to be more efficient for
-Svim to use simple awk commands
+Svim to use simple awk commands.
+
 Author: Diego Fuentes
 Contact email: diegofupa@gmail.com
 """
@@ -14,7 +17,7 @@ from __future__ import print_function
 
 import io
 import sys
-import argparse
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -41,26 +44,21 @@ def main():
     global plot
     plot = args.plot
     global minsup
-    minsup = args.min_support
+    minsup = args.MINSUP
     iterator = args.iterator
 
-    #Make sure the paths provided exist
+    #Make sure the paths provided exist and the iterator range is above min support reads previously filtered by sniffles.
     
-    try:
-        os.path.exists(hq_path)
-    except IOerror:
-        sys.stderr.write("eval_stats.py: error: The truth dataset path provided does not exist")
-        sys.exit(-1)
-    try:
-        os.path.exists(calls_path)
-    except IOerror:
-        sys.stderr.write("eval_stats.py: error: The variant call dataset path provided does not exist")
-        sys.exit(-1)
+    if not os.path.exists(hq_path):
+        raise OSError("Could not find {}.".format(hq_path))
 
-    if iterator >= 10:
+    if not os.path.exists(calls_path):
+        raise OSError("Could not find {}.".format(calls_path))
+
+    if iterator > minsup:
         pass
     else:
-        sys.stderr.write("eval_stats.py: error: argument --iterator: The iterator must surprass 10 to have an acceptable range")
+        sys.stderr.write("eval_stats.py: error: argument --iterator: The iterator must surprass the min support reads previously filtered by sniffles.")
         sys.exit(-1)
 
     #We set the truth dataset path
@@ -143,17 +141,17 @@ def main():
 
     #Create a eval_stats.txt file      
     file = open(str(date)+'_eval_stats_'+str(dataset)+'_'+str(feature)+'.txt', 'w')
-    file.write("These are the results for the evaluation of "+os.path.basename(hq)+" truth dataset and "+os.path.basename(calls)+" callset dataset for the specific feature "+str(feature)+".\n")
+    file.write("#These are the results for the evaluation of "+os.path.basename(hq)+" truth dataset and "+os.path.basename(calls)+" callset dataset for the specific feature "+str(feature)+".\n")
     file.write("\n")
     if plot == True:
-        file.write("Note that the added columns 'Mean values' and 'Std values' stand for the mean and standard deviation of the iterative loop using a range of 0 to "+str(iterator)+" scores.\n")
+        file.write("#Note that the added columns 'Average values' and 'Std values' stand for the mean and standard deviation of the iterative loop using a range of 0 to "+str(iterator)+" Q scores/read support.\n")
         results.insert(loc=2, column='Average values', value=[check_plot[0],check_plot[1], check_plot[2]])
         results.insert(loc=3, column='Std values', value=[check_plot[3],check_plot[4], check_plot[5]])
         file.write("\n")
         file.write(results.to_string(index=False))
     else:
         file.write(results.to_string(index=False))
-    file.write("\n\nAnd it is done! Bye.\n")
+    file.write("\n\n#And it is done! Bye.\n")
     file.close()
     print("\nRemoving the temporary files, please wait ...\n")
 
@@ -168,24 +166,36 @@ def svim_reformat(callset, featuretype, tempfile = 'svim_reformated.vcf'):
         """
         Reformat the svim vcf callset filtering by feature type first using awk commands.
         It was tested with bcftools view and vcffilter too but it performed faster with awk commands.
+
+        Params:
+        - callset: Str. Provided callset path.
+        - featuretype: Str. Provided Feature type to be filtered.
+        - tempfile = Str. Default name and extension of the temporary file which is going to be deleted by the end of this script.
         """
 
         command_svim_1 = 'cat ' + os.path.join(callset) + ' | awk \'OFS="\\t" {{ if($1 ~ /^#/) {{print $0}} else {{ if($5=="'+featuretype+'") {{ print $0 }} }}  }}\' > '+ \
         os.path.join(nwd, tempfile)
         print(os.popen(command_svim_1).read())
 
-        if os.path.exists(os.path.realpath(tempfile)):
-             return True
-        else:
-             print("eval_stats.py: error: The svim_reformat step didn't work")
-             sys.exit(-1)
+        #if os.path.exists(os.path.realpath(tempfile)):
+        #     return True
+        #else:
+        #     print("eval_stats.py: error: The svim_reformat step didn't work")
+        #     sys.exit(-1)
+        if not os.path.exists(os.path.realpath(tempfile)):
+             raise OSError("Could not find {}.".format(tempfile))
 
  
     
 def sniffles_reformat(callset, featuretype, tempfile ='sniffles_reformated.vcf'):
         """
-        Reformat the sniffles vcf callset filtering by feature type using vcffilter commands.
-        It was tested with bcftools view and awk too but it performed faster with awk commands
+        Reformat the sniffles vcf callset filtering by feature type using bcftools view commands.
+        It was tested with vcffilter and awk too but it performed faster with bcftools view.
+
+        Params:
+        - callset: Str. Provided callset path.
+        - featuretype: Str. Provided Feature type to be filtered.
+        - tempfile = Str. Default name and extension of the temporary file which is going to be deleted by the end of this script.
         """   
 
         #command_snif_1 = 'vcffilter -f \'SVTYPE = '+str(featuretype)+'\' '+ os.path.join(callset) + ' > ' + os.path.join(nwd, tempfile)
@@ -194,19 +204,34 @@ def sniffles_reformat(callset, featuretype, tempfile ='sniffles_reformated.vcf')
 
 
 
-        if os.path.exists(os.path.realpath(tempfile)):
-            return True
-        else:
-            print("eval_stats.py: error: The sniffles_reformat step didn't work")
-            sys.exit(-1)
+        #if os.path.exists(os.path.realpath(tempfile)):
+        #    return True
+        #else:
+        #    print("eval_stats.py: error: The sniffles_reformat step didn't work")
+        #    sys.exit(-1)
         #else:
             #pass
+        if not os.path.exists(os.path.realpath(tempfile)):
+             raise OSError("Could not find {}.".format(tempfile))
+
 def recall_precision_stats(truth, callset):
         """
-        Returns a table with the arithmetic 
+        Returns a table with the Recall, Precision and F1 scores when intersecting the high confidence dataset and the call dataset for a single SVTYPE.
+
+        Params:
+        - truth: Str. Provided hq dataset path.
+        - callset: Str. Provided callset path.
+
+        The measures above mentioned where calculated using the following formulas:
+        - Recall: TP/(TP+FN) . TP were any called sv that overlaps once with any sv of the truth dataset and FN were any of the truth dataset SVs left to be called.
+        - Precision: TP/(TP+FP) . TP were any called sv that overlaps once with any sv of the truth dataset and being FP any of the other called svs that did not overlap not even once
+        - F1 score: 2*((Recall*Precision)/(Recall+Precision)). It is the harmonic mean of precision and recall
+
+        Just to simplify, the TP+FN would be total number of variants for the same SVTYPE in the high confidence dataset and the TP+FP would be the total number of variants for the same
+        SVTYPE in the callset.
         """
         
-        #Total number of variants in the hq dataset
+        #Total number of variants in the high confidence (hq) dataset
         command_hq_1 =  'cat ' + os.path.abspath(truth) + ' | awk \'OFS="\\t" {{ if($1 !~ /^#/) {{print $0}} }}\' | wc -l'                           
         number_variants_hq = os.popen(command_hq_1).read()
                                                                   
@@ -242,23 +267,34 @@ def recall_precision_stats(truth, callset):
         print("TP for precision: "+str(call_vs_truth))
                                                                             
         #Evaluation Metrics:                                                                   
-        recall = (int(truth_vs_call)-1)/int(number_variants_hq) #TP/(TP+FN) considering that TP are any called sv that overlaps once with any sv of the truth dataset and being FN any of the truth dataset SVs left to be called
+        recall = (int(truth_vs_call)-1)/int(number_variants_hq) 
                                                                            
-        precision = (int(call_vs_truth)-1)/int(number_variants_callset)#TP/(TP+FP) considering that TP are any called sv that overlaps once with any sv of the truth dataste and being FP any of the other called svs that did not overlap not even once
+        precision = (int(call_vs_truth)-1)/int(number_variants_callset)
         
-        f1 = 2*((recall*precision)/(recall+precision))#harmonic mean of precision and recall
+        f1 = 2*((recall*precision)/(recall+precision))
                                                                            
         df = pd.DataFrame([[recall],
                      [precision], [f1]], 
                       columns = ['Results in proportion'])
     
-        df.insert(loc=0, column='Eval Metrics', value=['Sensitivity','Precision', 'F1'])
+        df.insert(loc=0, column='Eval Metrics', value=['Recall','Precision', 'F1'])
 
         #print(os.popen('rm intersect.temp.bed').read())
         
         return df
 def plot_filtering(truth, callset, featuretype, iterations=20):
-                                                                           
+        """
+        Plots an iterative loop of multiple stats based on the Recall, Precision and F1 score for multiple Q scores/Read support filtering. The plot is saved as .png.
+        Additionally it calculates the averages and the standard deviation for all 3 measures across the loop.
+
+        Params:
+        - truth: Str. Provided hq dataset path.
+        - callset: Str. Provided callset path.
+        - featuretype: Str. Provided Feature type to be filtered.
+        - iterations: int. Provided Q score/Read Support maximum to be iterated over to produce the plots and additional stats.
+
+        Returns a list with the averages and the standard deviation for all 3 measures.
+        """                                                                   
 
         performance_matrix = np.zeros((iterations,3))
         if sniffles == False:
@@ -266,10 +302,13 @@ def plot_filtering(truth, callset, featuretype, iterations=20):
             print("############################################")
             print("#Optional Step: Creating the plot#")                                                                  
             print("############################################\n")
+
+            #First we reformat in order to reduce computation time
             reformated = svim_reformat(callset, featuretype, tempfile = 'filter.temp.vcf')
+
+            #We set the iterative loop.
             for i in range(0, iterations, 1):
-                #i = i - 1 #To avoid issues with Python's indexing
-                #Recordar intentar sacar del loop el comando subcommand. Si solo genero un callset filtrado por featuretype, reduzco tiempo
+                print('\033[1m'+'Iteration: '+str(i+1)+'.'+'\033[0m')
                 #subcommand = 'grep -v \"hom_ref\" '+os.path.realpath(callset)+' | awk  \'OFS="\\t" {{ if($1 ~ /^#/) {{ print $0 }} else {{ if($6>='+str(i)+') {{ print $0 }} }} }}\' > '+os.path.join(nwd, 'filter.temp.vcf')
                 #subcommand = 'vcffilter -f \'QUAL > '+str(i+1)+'\' '+ os.path.join(callset) + ' > ' + os.path.join(nwd, 'filter.temp.vcf')
                 subcommand = 'grep -v \"hom_ref\" '+os.path.realpath('filter.temp.vcf')+' | awk  \'OFS="\\t" {{ if($1 ~ /^#/) {{ print $0 }} else {{ if($6>='+str(i)+') {{ print $0 }} }} }}\' > '+os.path.join(nwd, 'temp.vcf')
@@ -286,7 +325,7 @@ def plot_filtering(truth, callset, featuretype, iterations=20):
                 performance_matrix[i, 1] = precision
                 performance_matrix[i, 2] = f1
                 #Remove temp files
-                print(os.popen('rm temp.vcf').read(), 'Iteration: '+str(i+1)+'.')
+                print(os.popen('rm temp.vcf').read())
                 #print(os.popen('rm temp.vcf').read())
 
             print(os.popen('rm filter.temp.vcf').read())
@@ -322,14 +361,14 @@ def plot_filtering(truth, callset, featuretype, iterations=20):
             plt.grid()
             #Subplot 1
             plt.subplot(2,2,1)
-            plt.title("Sensitivity vs Precision trade-off")
+            plt.title("Recall vs Precision trade-off")
             line1, = plt.plot(X, Y, 'o-', color="b")
             plt.xlabel('Precision')
             plt.xticks(np.arange(0, 1.05, 0.05))
-            plt.ylabel('Sensitivity')
+            plt.ylabel('Recall')
             plt.yticks(np.arange(0, 1.05, 0.05))
             legend_handles = [ mlines.Line2D([], [], color='b', marker='o', \
-                            markersize=10, label='Sensitivity-Precision trade-off')]
+                            markersize=10, label='Recall-Precision trade-off')]
             plt.legend(handles=legend_handles, loc = 'best')
 
             #Subplot 2
@@ -348,16 +387,16 @@ def plot_filtering(truth, callset, featuretype, iterations=20):
             
             #Subplot 3
             plt.subplot(2,2,3)
-            plt.title("Sensitivity based on the Svim Q score filtering")
+            plt.title("Recall based on the Svim Q score filtering")
             line1, = plt.plot(np.arange(0, iterations), Y, 'o-', color="g")
             plt.fill_between(np.arange(0, iterations), Y - std_recall / np.sqrt(10), \
                          Y + std_recall / np.sqrt(10), alpha=0.1, color="green")
             plt.xlabel('Svim Q score selected')
             plt.xticks(np.arange(0, iterations, 2))
-            plt.ylabel('Sensitivity')
+            plt.ylabel('Recall')
             plt.yticks(np.arange(0, 1.05, 0.05))
             legend_handles = [ mlines.Line2D([], [], color='g',marker='o', \
-                            markersize=10, label='Sensitivity')]
+                            markersize=10, label='Recall')]
             plt.legend(handles=legend_handles, loc = 'best')
 
             #Subplot 4
@@ -390,7 +429,7 @@ def plot_filtering(truth, callset, featuretype, iterations=20):
 
 
             for i in range(0, iterations, 1):
-                #i = i - 1 #To avoid issues with Python's indexing
+                print('\033[1m'+'Iteration: '+str(i+1)+'.'+'\033[0m')
                 #subcommand = 'vcffilter -f \'SVTYPE = '+str(featuretype)+' & RE > '+str(i)+'\' '+ os.path.join(callset) + ' > ' + os.path.join(nwd, 'filter.temp.vcf')
                 subcommand = 'bcftools view -i \'INFO/RE>='+str(i)+'\' -O v '+os.path.join('filter.temp.vcf')+' > '+os.path.join(nwd, 'temp.vcf')
                 #subcommand = 'vcffilter -f \'RE > '+str(i)+'\' '+ os.path.join('filter.temp.vcf') + ' > ' + os.path.join(nwd, 'temp.vcf')
@@ -409,8 +448,7 @@ def plot_filtering(truth, callset, featuretype, iterations=20):
                 performance_matrix[i, 2] = f1
                 
                 #Remove temp files
-                print(os.popen('rm temp.vcf').read(), 'Iteration: '+str(i+1)+'.')
-                #print(os.popen('rm temp.vcf').read())
+                print(os.popen('rm temp.vcf').read())
 
             print(os.popen('rm filter.temp.vcf').read())
                 
@@ -445,14 +483,14 @@ def plot_filtering(truth, callset, featuretype, iterations=20):
             plt.grid()
             #Subplot 1
             plt.subplot(2,2,1)
-            plt.title("Sensitivity vs Precision trade-off")
+            plt.title("Recall vs Precision trade-off")
             line1, = plt.plot(X[minsup:,], Y[minsup:,], 'o-', color="b")
             plt.xlabel('Precision')
             plt.xticks(np.arange(0, 0.4, 0.05))
-            plt.ylabel('Sensitivity')
+            plt.ylabel('Recall')
             plt.yticks(np.arange(0.05, 1.05, 0.05))
             legend_handles = [ mlines.Line2D([], [], color='b', marker='o', \
-                            markersize=10, label='Sensitivity-Precision trade-off')]
+                            markersize=10, label='Recall-Precision trade-off')]
             plt.legend(handles=legend_handles, loc = 'best')
 
             #Subplot 2
@@ -471,16 +509,16 @@ def plot_filtering(truth, callset, featuretype, iterations=20):
             
             #Subplot 3
             plt.subplot(2,2,3)
-            plt.title("Sensitivity based on the Sniffles Read Support (RE) filtering")
+            plt.title("Recall based on the Sniffles Read Support (RE) filtering")
             line1, = plt.plot(np.arange(minsup, iterations), Y[minsup:,], 'o-', color="g")
             plt.fill_between(np.arange(minsup, iterations), Y[minsup:,] - std_recall / np.sqrt(10), \
                          Y[minsup:,] + std_recall / np.sqrt(10), alpha=0.1, color="green")
             plt.xlabel('Sniffles Read Support selected')
             plt.xticks(np.arange(minsup, iterations, 2))
-            plt.ylabel('Sensitivity')
+            plt.ylabel('Recall')
             plt.yticks(np.arange(0, 1.05, 0.05))
             legend_handles = [ mlines.Line2D([], [], color='g',marker='o', \
-                            markersize=10, label='Sensitivity')]
+                            markersize=10, label='Recall')]
             plt.legend(handles=legend_handles, loc = 'best')
 
             #Subplot 4
@@ -506,8 +544,8 @@ def plot_filtering(truth, callset, featuretype, iterations=20):
 
 def get_args():
 
-    parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]),
-                                 usage=__doc__)
+    parser = ArgumentParser(prog=os.path.basename(sys.argv[0]),
+                                 description=__doc__, formatter_class=RawDescriptionHelpFormatter)
     parser.add_argument('-t', '--truth', required=True,
                     help='VCF/BED high confidence ("truth") dataset')
     parser.add_argument('-c', '--callset', required=True,
@@ -516,12 +554,12 @@ def get_args():
                     help='Define the feature type to filter. '
                          'Default is <DEL>')
     parser.add_argument('-s', '--sniffles', action='store_true',
-                   help='Parameter used to reformat the sniffles callset if provided. Default is \'False\'.')
+                   help='Parameter used to reformat the sniffles callset if provided.')
     parser.add_argument('-p', '--plot', action='store_true',
-                   help='Parameter used to produce a plot of the eval metrics for different svim score filtering. Default is \'False\'.')
+                   help='Parameter used to produce a plot of the eval metrics for different svim score filtering.')
     parser.add_argument('-i', '--iterator', type=int, default=20,
                    help='Parameter used in the plotting step. It is the max svim filtering or sniffles supporting reads score range indicated from 0 to \'--iterator\'. Default is \'20\', though we recomend higher values for sniffles read support.')
-    parser.add_argument('-ms', '--min_support', type=int, default=10,
+    parser.add_argument('-ms', '--min_support', type=int, default=10, dest='MINSUP',
                    help='Parameter used in the plotting step. It is the min read support for sniffles. Default is \'10\'.')
     parser.add_argument('-v', '--verbose', action='store_true',
                     help='Verbose (goes to stderr)')
